da7af5951e750d8a1606ab89f2ea803173afeb05
Emilio Hoffmann <emiliohoffmann@hotmail.com>
2023-12-04 09:43:59 -0300

    Part one done

--- app/src/main/java/com/hoffmann/emilio/adventofcode2023/MainActivity.kt
+++ app/src/main/java/com/hoffmann/emilio/adventofcode2023/MainActivity.kt
@@ -3,12 +3,14 @@ package com.hoffmann.emilio.adventofcode2023
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hoffmann.emilio.adventofcode2023.one.One
+import com.hoffmann.emilio.adventofcode2023.two.Two
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        One.main(this)
+//        One.main(this)
+        Two.main(this)
     }
 }
---
+++ app/src/main/java/com/hoffmann/emilio/adventofcode2023/two/Two.kt
@@ -0,0 +1,72 @@
+package com.hoffmann.emilio.adventofcode2023.two
+
+import android.content.Context
+import com.hoffmann.emilio.adventofcode2023.R
+import com.hoffmann.emilio.adventofcode2023.utils.getStringListFromFile
+
+class Two {
+    companion object {
+        fun main(context: Context) {
+            val input = getStringListFromFile(R.raw.input_two, resources = context.resources)
+            val games = input.map { parseGame(it) }
+            val valid = games.filterNot { game ->
+                game.sets.any { set -> !set.isValidGame }
+            }
+            val sum = valid.sumOf { it.id }
+            println(sum)
+        }
+
+        data class Game(val id: Int, val sets: List<Set>)
+
+        data class Set(
+            val red: Int = 0,
+            val green: Int = 0,
+            val blue: Int = 0,
+            val isValidGame: Boolean
+        )
+
+        fun parseGame(string: String): Game {
+            val (idString, setsString) = string.split(": ")
+            val id = idString.trim().removePrefix("Game ").toInt()
+            val sets = setsString.split("; ").map { parseSet(it) }
+            return Game(id, sets)
+        }
+
+        fun parseSet(string: String): Set {
+            val items = string.split(", ")
+            var red = 0
+            var green = 0
+            var blue = 0
+            var isValid = true
+            for (item in items) {
+                val (numberOfDies, color) = item.split(" ")
+                when (color) {
+                    "red" -> {
+                        val x = numberOfDies.toIntOrNull() ?: 0
+                        if (x > 12) {
+                            isValid = false
+                        }
+                        red = x
+                    }
+
+                    "green" -> {
+                        val x = numberOfDies.toIntOrNull() ?: 0
+                        if (x > 13) {
+                            isValid = false
+                        }
+                        green = x
+                    }
+
+                    "blue" -> {
+                        val x = numberOfDies.toIntOrNull() ?: 0
+                        if (x > 14) {
+                            isValid = false
+                        }
+                        blue = x
+                    }
+                }
+            }
+            return Set(red, green, blue, isValid)
+        }
+    }
+}
